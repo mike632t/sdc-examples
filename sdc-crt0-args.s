0e2535013e8f5964c7386f5b3de2bb7be059f326
mike632t <mike632t@gmail.com>
2023-09-06 23:29:52 +0100

    Defined a seperate stack for the program. Checks how many arguments have been parsed. Uses the memory occupied by the inital code to store the pointers to each command line argument.

--- sdc-crt0-args.s
+++ sdc-crt0-args.s
@@ -35,8 +35,15 @@
 ;** 01 Sep 23   - Added  a  check for the CPU type using the overflow  flag
 ;		  which behaves differently on the 8080 - MT
 ;
-;** To Do	- Check number of arguments doesn't overflow tha array!
-;
+;** 06 Sep 23	- Defined a seperate stack for the program (otherwise using
+;                 functions like printf() on CP/M causes it to crash) - MT
+;               - Checks how many arguments have been parsed to prevent the
+;                 array of pointed over writing the parsing code - MT
+;               - Uses the memory occupied by the inital code to store  the
+;                 pointers to each command line argument, this is sufficent
+;                 to handle up to 34 arguments.  Should the command line be
+;                 longer  than this any remaining characters will be passed
+;                 to main() along with the last argument - MT
 ;
 		.module	crt0
 
@@ -44,24 +51,23 @@
 
 		.area	_HEADER (ABS)
 		.org	0x0100
-
+args:					; Overwriting initial code with arguments allows up to 34 arguments be passed to the main program.
+		jp	start
+err_msg:
+		.str	"Z80 processor required."
+		.db	00,13,10,'$'
+start:
 		ld	a,#0x7f		; Load with largest positive signed value.
 		inc	a		; Incrementing should result in an overflow.
 		jp	pe,parse	; Z80 processor set the parity flag to signify overflow (8080 doesn't).
 		ld	de,#err_msg	; Display error message.
 		ld	c,#0x09		; Print string.
 		jp	0x0005		; Jump to BDOS (when BDOS returns program will exit).
-err_msg:
-		.str	"Z80 processor required."
-		.db	00,13,10,'$'
-args:
-		.dw	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-		.dw	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 parse:
 		ld	hl,#0x80	; Pointer to command line.
 		ld	bc,(#0x80)	; Length of command line.
 		xor	a
-		ld	b,a		
+		ld	b,a
 		add	hl,bc
 		inc	hl		; Offset to end of command line.
 		ld	(hl),#0x00	; Terminate commans line with a null.
@@ -77,10 +83,10 @@ scan:
 		or	a		; Set flags
 		jr	z,done
 		cp	#' '
-		jr	nz,scan		
+		jr	nz,scan
 		xor	a
 		ld	(hl),a		; Terminate parameter.
-loop:		
+loop:
 		inc	hl		; Move to next char in buffer
 		ld	a,(hl)
 		or	a		; Set flags
@@ -95,18 +101,27 @@ save:
 		inc	hl		; Increment pointer to argv[]
 		ex	de,hl
 		inc	bc		; Increment number of arguments.
+		ld	a,c
+		cp	#35
+		jr	z,done		; Stop parsing after 34 arguments!
 		jr	scan
 
-done:		xor	a
-		ld	h,b		; Number of arguments.
+done:
+		ld 	(stack),sp	; Save the stack pointer.
+		ld	sp,#stack
+		xor	a
+		ld	h,a		; Number of arguments.
 		ld	l,c
 		ld	de,#args	; Pointer to arguments.
-
-		call	_main		; Jump to the C main routine
+		call	_main		; Call the C main routine
+		ld	sp,(stack)	; Restore original stack pointer
 		ret
 
 ;		.area	_TPA		; Ordering of segments for the linker.
 ;		.area	_HOME
 		.area	_CODE
 		.area	_DATA
+		.ds	256
+stack:
+		.dw	0
 ;
