20aa8e0404779da5195da8e83fa3bcbbfaa6cecf
mike632t <1799044+mike632t@users.noreply.github.com>
2024-11-10 21:23:11 +0000

    Added the obligatory hello world example

---
+++ sdc-hello.c
@@ -0,0 +1,50 @@
+/*
+ * hello.c
+ *
+ * Copyright(C) 2023   MT
+ *
+ * Classic hello world program.
+ * 
+ * For sdcc compile using:
+ *    sdasz80 -o sdc-crt0.s
+ *    sdcc -mz80 -c sdc-cpm.c
+ *    sdcc -mz80 --no-std-crt0 --data-loc 0 sdc-crt0.rel sdc-cpm.rel sdc-hello.c 
+ *    sdobjcopy -Iihex -Obinary --gap-fill 0 sdc-hello.ihx sdc-hello.com
+ * 
+ * This  program is free software: you can redistribute it and/or modify it
+ * under  the terms of the GNU General Public License as published  by  the
+ * Free  Software Foundation, either version 3 of the License, or (at  your
+ * option) any later version.
+ *
+ * This  program  is distributed in the hope that it will  be  useful,  but
+ * WITHOUT   ANY   WARRANTY;   without even   the   implied   warranty   of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You  should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 20 Aug 23         - Initial version - MT
+ * 
+ * ToDo
+ *
+ */
+
+#define  NAME        "sdc-hello"
+#define  VERSION     "0.1"
+#define  BUILD       "0001"
+#define  AUTHOR      "MT"
+#define  COPYRIGHT   (__DATE__ + 7) /* Extract copyright year from date */
+ 
+#include <stdio.h>
+#include "sdc-cpm.h"
+
+void main()
+{
+   int i_count;
+   
+   for (i_count = 0; i_count <= 9; i_count++)
+   {
+      puts("Hello World !!");
+   }
+}
