6628d8c53e95e727ee3ecb4e83e461c64a48f13d
mike632t <1799044+mike632t@users.noreply.github.com>
2024-11-24 21:11:22 +0000

    Inserted a jump instruction at the beginning of the program and exits via a CP/M reset

--- sdc-crt0-args.s
+++ sdc-crt0-args.s
@@ -28,143 +28,144 @@
 ;   You should have received a copy of the GNU General Public License along
 ;   with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;
-;** 22 Nov 24   0.1	- Initial version - MT 
-;			- Added check CPU type - MT
-;			- Arguments are now now ANSI compliant (returning a 
-;                         null string for arg[0] is valid) - MT
+;** 22 Nov 24   0.1   - Initial version - MT 
+;                     - Added check CPU type - MT
+;                     - Arguments  are now now ANSI compliant (returning  a 
+;                       null string for arg[0] is valid) - MT
 ;
-;   To Do:		- Allow  command line parameters to be enclosed  in
-;			  quotes.
+;** 24 Nov 24         - Exit  via a CP/M reset to avoid any stack  overflow 
+;                       issues - MT
+;                     - Inserted a jump instruction at the beginning of the 
+;                       program and tidied up the comments - MT
 ;
-			.title	crt0-args.s
+;   To Do:            - Allow  command  line parameters to be  enclosed  in
+;                       quotes.
 ;
-			.globl	_main
-			.globl	_bdos
+                .module crt0
 ;
-			.area 	_HEADER (ABS)
+                .globl  _main
 ;
-			.org	0x0100			; Start address.
+                .area   _HEADER (ABS)
+                .org    0x0100
 ;
 ;-- Check CPU type (as sdcc requires a Z80)
 ;
-start:			ld	a,#0x7f			; Load with largest positive signed value.
-			inc	a			; Incrementing should result in an overflow.
-			jp	pe,init			; Z80 processor set the parity flag to signify overflow (8080 doesn't).
-			ld	de,#err_msg		; Display error message.
-			ld	c,#0x09			; Print string.
-			jp	0x0005			; Jump to BDOS (when BDOS returns program will exit).
+                jp      start           ; Jump the start of program.
+;
 err_msg:
-			.str	"Z80 processor required."
-			.db	13,10,'$'
+                .str    "Z80 processor required."
+                .db     13,10,'$'
 ;
-;-- Check if there are any parameters
+start:          ld      a,#0x7f         ; Load with largest positive signed value.
+                inc     a               ; Incrementing should result in an overflow.
+                jp      pe,init         ; Z80 processor set the parity flag to signify overflow (8080 doesn't).
+                ld      de,#err_msg     ; Display error message.
+                ld      c,#0x09         ; Print string.
+                jp      0x0005          ; Jump to BDOS (when BDOS returns program will exit).
 ;
-init:			ld	a,(#0x80)
-			or	a
-			ld	c,#0
-			jr	z,cont
+init:           ld      a,(#0x80)
+                or      a
+                ld      c,#0
+                jr      z,cont
 ;
 ;-- Terminate command line with an ASCII NUL this will tell us when to stop 
 ;   scanning and terminate the last argument.
 ;
-			ld	hl,#0x81
-			ld	bc,(#0x80)
-			ld	b,#0
-			add	hl,bc
-			ld	(hl),#0
-;			
+                ld      hl,#0x81
+                ld      bc,(#0x80)
+                ld      b,#0
+                add     hl,bc
+                ld      (hl),#0
+;                       
 ;-- Initialize registers and jump to the loop routine.
-;			
-			xor     a
-			ld	c,a		; Clear number of parameters.
-			ld      hl,#0x80	; Address of the command tail.
-			ld      (hl),a		; Make argv[0] a nul string.
+;                       
+                xor     a
+                ld      c,a             ; Clear number of parameters.
+                ld      hl,#0x80        ; Address of the command tail.
+                ld      (hl),a          ; Make argv[0] a nul string.
 
-			ld      ix,#0x0100	; Over writes the CPU type checking.
+                ld      ix,#0x0100      ; Over writes the CPU type checking.
 ;
 ;-- Add argv[0] add its address to array.
-;			
-			ld	(ix),l
-			ld	1(ix),h
-			inc	ix
-			inc	ix
-			inc	c
-			ld      hl,#0x81	; Address of command tail
-			call	parser
-;			
+;                       
+                ld      (ix),l
+                ld      1(ix),h
+                inc     ix
+                inc     ix
+                inc     c
+                ld      hl,#0x81        ; Address of command tail
+                call    parse
+;                       
 ;-- Command line processing done.
 ;
-cont:			ld	hl,#0x0100	
-			ld	b,#0		; C contains the number of arguments.
-			push	hl		; Pass info as parameters to "main"
-			push	bc
+cont:           ld      hl,#0x0100      
+                ld      b,#0            ; C contains the number of arguments.
+                push    hl              ; Pass info as parameters to "main"
+                push    bc
 ;
 ;-- Call the "main" function
 ;
-			push	de
-			ld	de,#_HEAP_start	; Save the address of the heap 
-			ld 	(_heap_top),de
-			pop 	de
-
-			call	_main
+                push    de
+                ld      de,#_HEAP_start ; Save the address of the heap 
+                ld      (_heap_top),de
+                pop     de
+;
+                call    _main
 ;
 ;-- Exit program when main is finished.
 ;
-			pop	bc		; Unload the stack
-			pop	hl
-			ret			; and return.
+;               pop     bc              ; Unload the stack
+;               pop     hl
+;               ret                     ; and return.
 ;
 ;-- Alternatively perform a warm reset.
 ;
-;			ld	c,#0		; Call BDOS RESET function
-;			jp	5
-;			
+                ld      c,#0            ; Call BDOS RESET function
+                jp      5
+;                       
 ;-- Begin processing the command line
 ;
-parser:			ld	a,(hl)
-			or	a		; Command line end found?
-			ret	z
+parse:          ld      a,(hl)
+                or      a               ; Command line end found?
+                ret     z
 
-			cp	#32		; Skip spaces
-			jr	nz,next
-			inc	hl
-			jr	parser
+                cp      #32             ; Skip blank spaces.
+                jr      nz,next
+                inc     hl
+                jr      parse
 ;
 ;-- Found another argument so add its address to array.
 ;
-next:			ld	(ix),l		; Copy address to location pointed to by ix.
-			ld	1(ix),h
-			inc	ix		; Increment the pointer.
-			inc	ix
-			inc	c		; Increment the counter.
-			
-			ld	a,c		; Check the number of parameters
-			cp	#10		; Max 16 parameters... 
-			ret	nc		; Give up if there are too many.
+next:           ld      (ix),l          ; Copy address to location pointed to by ix.
+                ld      1(ix),h
+                inc     ix              ; Increment the pointer.
+                inc     ix
+                inc     c               ; Increment the counter.
+                
+                ld      a,c             ; Check the number of parameters
+                cp      #10             ; Max 16 parameters... 
+                ret     nc              ; Give up if there are too many.
 ;
-;-- ...and skip chars until you find a space or the end of the command line.
+;-- Skip chars until you find a space or the end of the command line.
 ;
-loop:			ld	a,(hl)
-			or	a		; Command line end found?
-			ret	z
+loop:           ld      a,(hl)
+                or      a               ; Command line end found?
+                ret     z
 
-			cp	#32
-			jr	nz,nospc
-			ld	(hl),#0		; If space found, set it to zero (string terminator)...
-			inc	hl
-			jr	parser		; ...and start again.
+                cp      #32
+                jr      nz,nospc
+                ld      (hl),#0         ; If space found, set it to zero (string terminator)...
+                inc     hl
+                jr      parse           ; ...and start again.
 
-nospc:			inc	hl
-			jr	loop
+nospc:          inc     hl
+                jr      loop
 ;
-;-- Place data after program code, and data init code after data
-
-			.area	_CODE		; Program code area
-			.area	_DATA		; Data area
-_heap_top::		.dw 0			; Address of the start of the heap area
-
-;			.area	_OVERLAY	; These areas don't seem to be required.
-;			.area	_HOME
-;			.area	_BSS
-			.area	_HEAP
-_HEAP_start::					; Space for the heap.
+;-- Place data after program code, and heap after data
+;
+                .area   _CODE           ; Program code area
+                .area   _DATA           ; Data area
+_heap_top::     .dw 0                   ; Address of the start of the heap area
+;
+_HEAP_start::                           ; Space for the heap.
+;
