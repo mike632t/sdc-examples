bc19600cbc153b21c2dd3c505f156a2ab56c85a7
mike632t <1799044+mike632t@users.noreply.github.com>
2026-09-26 00:56:38 +0100

    Use constants defined in sdc-cpm.h

--- sdc-cpm.c
+++ sdc-cpm.c
@@ -32,43 +32,7 @@
 #define  AUTHOR      "MT"
 #define  COPYRIGHT   (__DATE__ + 7) /* Extract copyright year from date. */
 
-#define S_RESET		0
-#define C_READ       1
-#define C_WRITE      2
-#define A_READ       3
-#define A_WRITE      4
-#define L_WRITE      5
-#define C_RAWIO      6
-#define GETIOBYTE    7
-#define SETIOBYTE    8
-#define C_WRITESTR   9
-#define C_READSTR    10
-#define DRV_ALLRESET 13
-#define DRV_SET      14
-#define F_OPEN       15
-#define F_CLOSE      16
-#define F_SMATCH     17
-#define F_NMATCH     18
-#define F_DELETE     19
-#define F_READ       20
-#define F_WRITE      21
-#define F_MAKE       22
-#define F_RENAME     23
-#define DRV_LOGINVEC 24
-#define DRV_GET      25
-#define F_DMAOFF     26
-#define DRV_ALLOCVEC 27
-#define DRV_SETRO    28
-#define DRV_ROVEC    29
-#define F_ATTRIB     30
-#define DRV_DPB      31
-#define F_USERNUM    32
-#define F_READRAND   33
-#define F_WRITERAND  34
-#define F_SIZE       35
-#define F_RANDREC    36
-#define DRV_RESET    37
-#define F_WRITEZF    40
+#include "sdc-cpm.h"
 
 #if __SDCCCALL /* Use new calling convention */
 
